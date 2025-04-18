9357fbca9f12396ce917e8a61d112e3e407ed604
Athenkosi Mamfengu <53196964+Athistic@users.noreply.github.com>
2025-04-18 13:47:48 +0200

    Created certficate to allow app to run on a trusted https server for GetUser api to be able to send cookies back to the Backend cause it cant send the cookie to an http only server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/Chat-app-FE/',
+  server: {
+    https: {
+      key: fs.readFileSync(
+        path.resolve(
+          '/Users/athenkosi.mamfenguintegrove.com/Desktop',
+          'key.pem'
+        )
+      ),
+      cert: fs.readFileSync(
+        path.resolve(
+          '/Users/athenkosi.mamfenguintegrove.com/Desktop',
+          'cert.pem'
+        )
+      ),
+    },
+  },
 });
