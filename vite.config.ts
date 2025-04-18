d0c99b09e39009e5147e994dd873290206db3f40
Athenkosi Mamfengu <53196964+Athistic@users.noreply.github.com>
2025-04-18 14:07:54 +0200

    fix cert issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,18 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import fs from 'fs';
 
-// https://vite.dev/config/
+// Check if we're in development mode
+const isLocalDev = process.env.NODE_ENV !== 'production';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/Chat-app-FE/',
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
-    },
-  },
+  server: isLocalDev
+    ? {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+        },
+      }
+    : undefined, // No HTTPS config in production
 });
